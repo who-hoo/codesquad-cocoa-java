515e2694452571d8ec458773a4705554015aa420
who-hoo <who.ho3ov@gmail.com>
2021-11-08 16:56:54 +0900

    Fix: 수정&삭제 시 전체 잔액 리밸런싱, 월별 잔액 계산 로직 추가

--- week02/src/day06/mission3/accountbook/AccountBook.java
+++ week02/src/day06/mission3/accountbook/AccountBook.java
@@ -53,6 +53,8 @@ public class AccountBook {
                 isRunning = false;
                 input.close();
                 break;
+            default:
+                break;
         }
     }
 
@@ -80,6 +82,7 @@ public class AccountBook {
             System.out.println("해당 순번의 데이터가 존재하지 않습니다.");
             return;
         }
+        AccountData.balance -= (target.income - target.expense);
         String result = contents.remove(target) ? "delete success" : "delete fail";
         System.out.println(result);
     }
@@ -90,35 +93,38 @@ public class AccountBook {
             System.out.println("해당 순번의 데이터가 존재하지 않습니다.");
             return;
         }
+        AccountData.balance -= (target.income - target.expense);
+
         int targetIndex = contents.indexOf(target);
-
-        String date = input.getYYYYMMDD();
-        target.date = date;
-
-        String summary = input.getString("적요 >>>>> ");
-        target.summary = summary;
-
-        int income = input.getInteger("수입 >>>>> ");
-        target.income = income;
-
-        int expense = input.getInteger("지출 >>>>> ");
-        target.expense = expense;
-
+        target.date = input.getYYYYMMDD();
+        target.summary = input.getString("적요 >>>>> ");
+        target.income = input.getInteger("수입 >>>>> ");
+        target.expense = input.getInteger("지출 >>>>> ");
         AccountData result = contents.set(targetIndex, target);
+        AccountData.balance += (target.income - target.expense);
         System.out.printf("update success : [%d] %s %s %d %d %n"
             , result.no, result.date, result.summary, result.income, result.expense);
     }
 
+    int calcMonthlyBalance(int month) {
+        return contents.stream()
+            .filter(
+                content -> Integer.parseInt(content.date.substring(4, 6)) == (month == 0 ?
+                    Integer.parseInt(content.date.substring(4, 6)) : month))
+            .mapToInt(content -> content.income - content.expense).sum();
+    }
+
     void printContents(int month) {
         System.out.println("========== " + (month == 0 ? "전체" : month) + " 월의 지출내역 출력 ==========");
-        System.out.printf("[순번] 날짜 적요 수입 지출 잔액 %n");
+        System.out.printf("[순번] 날짜 적요 수입 지출 %n");
         contents.stream()
             .filter(
                 content -> Integer.parseInt(content.date.substring(4, 6)) == (month == 0 ?
                     Integer.parseInt(content.date.substring(4, 6)) : month))
-            .forEach(content -> System.out.printf("[%d] %s %s %d %d %d %n"
-                , content.no, content.date, content.summary, content.income, content.expense,
-                AccountData.balance));
+            .forEach(content -> System.out.printf("[%d] %s %s %d %d %n"
+                , content.no, content.date, content.summary, content.income, content.expense));
+        System.out.println("=======================================");
+        System.out.println("잔액 : " + calcMonthlyBalance(month) + "원, 총잔액 : " + AccountData.balance);
     }
 
     public static void main(String[] args) {
