a04946f4294c1500e39ca5475460b6f119093073
who-hoo <who.ho3ov@gmail.com>
2021-11-10 12:07:47 +0900

    Feat: AccountData에 결제유형(paymentType) 추가

--- week02/src/day06/mission3/accountbook/AccountData.java
+++ week02/src/day06/mission3/accountbook/AccountData.java
@@ -1,18 +1,23 @@
 package day06.mission3.accountbook;
 
+import day06.mission3.accountbook.meta.PaymentType;
+
 public class AccountData {
 
-    int no;
+    final int no;
     String yyyymmdd;
     String summary;
     int income;
     int expense;
+    PaymentType paymentType;
 
-    AccountData(int no, String date, String summary, int income, int expense) {
+    AccountData(int no, String date, String summary, int income, int expense,
+        PaymentType paymentType) {
         this.no = no;
         this.yyyymmdd = date;
         this.summary = summary;
         this.income = income;
         this.expense = expense;
+        this.paymentType = paymentType;
     }
 }
