b97a11ceb526cd9c80ac393a778408b4d67eaf4f
who-hoo <who.ho3ov@gmail.com>
2021-11-24 16:15:00 +0900

    Refac: pick 버튼 접근제어자 누락된거 추가

--- week04/src/gui/RandomPick.java
+++ week04/src/gui/RandomPick.java
@@ -11,7 +11,7 @@ public class RandomPick {
     private final TextField nameInput = new TextField("코코아 멤버의 이름을 입력하세요.", 50);
     private final Button addBtn = new Button("Add");
     private final TextArea membersView = new TextArea("", 0, 0, TextArea.SCROLLBARS_NONE);
-    Button pickBtn = new Button("Pick");
+    private final Button pickBtn = new Button("Pick");
 
     public void init() {
         Toolkit tk = Toolkit.getDefaultToolkit();
