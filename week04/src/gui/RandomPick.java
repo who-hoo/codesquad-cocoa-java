6336f2114815ad33c17e6e00e82e98c8cc136e9f
who-hoo <68011320+who-hoo@users.noreply.github.com>
2021-11-25 00:28:01 +0900

    Feat: Day18 미션코드 제출
    * Update: import package, 오타 수정
    
    * Feat: Frame 생성
    
    * Feat: 텍스트영역(이름 입력, 이름 목록)과 버튼(추가) 프레임에 추가
    
    * Feat: X 눌렀을 때 종료하도록 이벤트 처리
    
    * Refac: 컴포넌트들 init메서드 내 지역변수에서 인스턴스 변수로 변경
    
    * Feat: Add 버튼 클릭 시, 해당 내용 텍스트영역에 추가하도록 이벤트 처리
    
    * Feat: 엔터키 누르면 해당 내용 텍스트영역에 추가하도록 이벤트 처리
    
    * Feat: Pick 버튼 추가
    
    * Feat: Pick 버튼 클릭 이벤트 처리
    
    * Refac: 불필요한 import문 제거
    
    * Style: 프레임 사이즈 수정
    
    * Refac: pick 버튼 접근제어자 누락된거 추가
    
    * Refac: KeyListener -> ActionListener로 변경
    
    * Feat: 리셋버튼추가(기능까지)

---
+++ week04/src/gui/RandomPick.java
@@ -0,0 +1,72 @@
+package gui;
+
+import java.awt.*;
+import java.util.Random;
+
+public class RandomPick {
+
+    private final Frame f = new Frame("Random Pick");
+    private final TextField nameInput = new TextField("코코아 멤버의 이름을 입력하세요.", 50);
+    private final Button addBtn = new Button("Add");
+    private final TextArea membersView = new TextArea("", 0, 0, TextArea.SCROLLBARS_NONE);
+    private final Button resetBtn = new Button("Reset");
+    private final Button pickBtn = new Button("Pick");
+
+    public void init() {
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension screenSize = tk.getScreenSize();
+
+        f.setLocation(screenSize.width / 2 - 150, screenSize.height / 2 - 100);
+        f.setSize(500, 300);
+        f.setResizable(false);
+        f.setLayout(new FlowLayout());
+        f.addWindowListener(new EventHandler());
+
+        f.add(nameInput);
+        nameInput.addActionListener(e -> {
+            String name = nameInput.getText();
+            membersView.append(name + "\n");
+            nameInput.setText("");
+        });
+
+        f.add(addBtn);
+        addBtn.addActionListener(e -> {
+            String name = nameInput.getText();
+            membersView.append(name + "\n");
+            nameInput.setText("");
+        });
+
+        membersView.setEditable(false);
+        f.add(membersView);
+
+        f.add(resetBtn);
+        resetBtn.addActionListener(e -> membersView.setText(""));
+
+        f.add(pickBtn);
+        pickBtn.addActionListener(e -> {
+            String[] members = membersView.getText().split("\n");
+            String pickedMember = members[new Random().nextInt(members.length)];
+            Dialog result = new Dialog(f, "Result", true);
+            result.setSize(140, 90);
+            result.setLocation(50, 50);
+            result.setLayout(new FlowLayout());
+            Label msg = new Label("랜덤 뽑기 결과 : " + pickedMember, Label.CENTER);
+            Button ok = new Button("OK");
+            result.add(msg);
+            result.add(ok);
+
+            ok.addActionListener(okEvent -> {
+                result.setVisible(false);
+                result.dispose();
+            });
+
+            result.setVisible(true);
+        });
+
+        f.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new RandomPick().init();
+    }
+}
--- week04/src/jungsuk/ch14/practice/Exercise14_04.java
+++ week04/src/jungsuk/ch14/practice/Exercise14_04.java
@@ -1,14 +1,17 @@
 package jungsuk.ch14.practice;
 
+import java.util.Arrays;
 import java.util.function.BiConsumer;
 import java.util.function.BiPredicate;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Exercise14_04 {
 
     public static void main(String[] args) {
         Stream<Integer> die = IntStream.rangeClosed(1, 6).boxed();
         
-        die.flatMap(i -> Stream.of(1, 2, 3, 4, 5, 6).map(i2 -> new int[] { i, 23 }))
+        die.flatMap(i -> Stream.of(1, 2, 3, 4, 5, 6).map(i2 -> new int[] { i, i2 }))
             .filter(iArr -> iArr[0] + iArr[1] == 6)
             .forEach(iArr -> System.out.println(Arrays.toString(iArr)));
     }
