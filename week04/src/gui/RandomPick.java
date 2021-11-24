9c97210cfb4cc64f8b47232ac9272eb1119fe1af
who-hoo <who.ho3ov@gmail.com>
2021-11-24 14:42:36 +0900

    Feat: 텍스트영역(이름 입력, 이름 목록)과 버튼(추가) 프레임에 추가

--- week04/src/gui/RandomPick.java
+++ week04/src/gui/RandomPick.java
@@ -12,6 +12,18 @@ public class RandomPick {
         f.setLocation(screenSize.width / 2 - 150, screenSize.height / 2 - 100);
         f.setSize(500, 500);
         f.setResizable(false);
+        f.setLayout(new FlowLayout());
+
+        TextField nameInput = new TextField("코코아 멤버의 이름을 입력하세요.", 50);
+        f.add(nameInput);
+
+        Button addBtn = new Button("Add");
+        f.add(addBtn);
+
+        TextArea membersView = new TextArea("", 0, 0, TextArea.SCROLLBARS_NONE);
+        membersView.setEditable(false);
+        f.add(membersView);
+
         f.setVisible(true);
     }
 
